730ffb50c209f3a0626bf8eb8fbdba5c8ab38988
Your Name <you@example.com>
2023-01-06 02:13:06 -0800

    updated readme and some logic

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 https://playwright.dev/python/docs/intro
 
 Ubuntu Setup
-```
+```bash
 sudo apt update
 sudo apt install python3-pip
 pip install pytest-playwright
@@ -13,8 +13,17 @@ playwright install
 playwright install-deps
 ```
 
-Running playwright codegen
+Running the app
+```bash
+# Run app
+python3 zoombot.py
 ```
+
+Running playwright codegen
+```bash
 # playwright codegen <site>
 playwright codegen https://unr.zoom.us/j/83958869209?uname=Jann+Arellano
 ```
+
+WSL2 can run GUI apps natively now according to this https://learn.microsoft.com/en-us/windows/wsl/tutorials/gui-apps
+as long as it's Windows 11 or a high version of Windows 10 + Windows Insider (and updated drivers)
--- zoombot.py
+++ zoombot.py
@@ -2,19 +2,19 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, )
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://unr.zoom.us/j/83958869209?uname=Jann+Arellano")
-    page.goto("https://unr.zoom.us/j/83958869209?uname=Jann+Arellano#success")
+    # page.goto("https://unr.zoom.us/j/83958869209?uname=Jann+Arellano")
+    page.goto("https://tmcc-edu.zoom.us/j/5867770981")
+    
     page.get_by_role("button", name="Join from Your Browser").click()
-    page.get_by_placeholder("Your Name").click()
     page.get_by_placeholder("Your Name").fill("Jann Arellano")
     page.locator("#input-group").click()
     page.locator("#input-group").click()
     page.get_by_placeholder("Your Name").click()
     page.get_by_role("button", name="Join").click()
-    page.goto("https://unr.zoom.us/wc/83958869209/join?track_id=&jmf_code=&meeting_result=&tk=&cap=undefined&refTK=&rn=false&po=7&epk=weKtDjKG9S22q5bx5QJkvNoOnnFdk88LwKBIXPL9cdX72Wf7U-0.Eq9Mlk2GpWP3LM_W")
+    frame.wait_for_timeout(5000)
 
     # ---------------------
     context.close()
