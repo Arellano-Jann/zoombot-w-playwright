bed81563924c7e09e7f81e00da198a4242d1c2d0
Your Name <you@example.com>
2023-01-09 03:36:49 -0800

    config updates

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
+# Timezone setup
+# minute = 0 # 00 PST
+hour = "18:00" # 18 # 6:00 PM PST
+# days = [0, 2, 4] # Mon, Wed, Fri
+
+# Timing info
+# time_in_meeting = 60*1.75*60 # 60 min * 1.75 hours * 60 seconds
+# time_in_meeting = 60*2*60 # 60 min * 2 hours * 60 seconds
 time_in_meeting = 30 # in seconds
 
 # Links
-meeting_link = "https://tmcc-edu.zoom.us/j/5867770981#success"
+meeting_link = "https://tmcc-edu.zoom.us/j/5867770981#success" # test link, note that '#success' skips the download and popup. i don't know how to get past this without a link redirect
+# meeting_link = "https://learn.techwise.talentsprint.com/extras/join_zoom_meeting?meeting_id=92084655704"
+# meeting_link = "https://unr.zoom.us/j/83958869209?uname=Jann+Arellano"
 
 # Login Info
 # nameTag = "Jann Arellano"
