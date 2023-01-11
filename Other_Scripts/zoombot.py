e9cc74530e171820ef0f73177adf1d46a8e93a24
Your Name <you@example.com>
2023-01-11 06:16:02 -0800

    edietd other scripts

--- Other_Scripts/zoombot.py
+++ Other_Scripts/zoombot.py
@@ -4,7 +4,8 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False, slow_mo=5000)
+    # browser = playwright.chromium.launch(headless=False, slow_mo=5000)
+    browser = playwright.chromium.launch()
     context = browser.new_context()
     page = context.new_page()
     # page.goto("https://unr.zoom.us/j/83958869209?uname=Jann+Arellano")
